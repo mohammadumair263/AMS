498827b29e9f7c41a0658d6d3a857a80b09dbbc5
mohammadumair263 <mohammadumair263@gmail.com>
2019-04-12 16:04:12 +0500

    migrations issue resolved

--- database/migrations/2019_04_11_175443_create_attendances_table.php
+++ database/migrations/2019_04_11_175443_create_attendances_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAttendanceTable extends Migration
+class CreateAttendancesTable extends Migration
 {
     /**
      * Run the migrations.
